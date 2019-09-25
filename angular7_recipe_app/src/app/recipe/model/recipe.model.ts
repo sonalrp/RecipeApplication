af9aa75a0f5a2eb53aa189f8d9de5dd054411e16
Sonal_RP <sonalparsawar210@gmail.com>
2019-09-25 19:09:30 +0530

    Angular Initial Commit

--- angular7_recipe_app/src/app/recipe/model/recipe.model.ts
+++
@@ -1,9 +0,0 @@
-export class Recipe {
-    //recipeId: String;
-    recipeName: string;
-    creationdatenadtime: string;
-    veg: string;
-    noofppl: string;
-    listofingrediants: string;
-    cookinginstructions: string;
-}
